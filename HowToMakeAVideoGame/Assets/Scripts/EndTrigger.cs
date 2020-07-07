7df0a4c15682a3d6aaaa518861cec1c708c28f41
Duygu Üçer <32361271+duygucer@users.noreply.github.com>
2020-07-07 17:16:44 +0300

    CUBETHON-GAME
    Developing the tutorial project of Brackeys
    
    You can find the .exe file in the Builds folder. The demo version is a game. It consists of only 4 levels. The only difference from the Brackeys video series: in my version, blocks move towards player, not player towards blocks!

---
+++ HowToMakeAVideoGame/Assets/Scripts/EndTrigger.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class EndTrigger : MonoBehaviour
+{
+    public GameManager gameManager;
+    public Transform endLevelO;
+    void FixedUpdate()
+    {
+        if (endLevelO.position.z <= 25)
+        {
+            gameManager.CompleteLevel();
+        }
+    }
+
+
+}
---
+++ HowToMakeAVideoGame/Assets/Scripts/score.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+public class score : MonoBehaviour
+{
+    public Transform endPosition;
+    public Text scoreText;
+    int maxScore;
+    void Start()
+    {
+        maxScore = Convert.ToInt32(endPosition.position.z);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = (maxScore-endPosition.position.z).ToString("0");
+    }
+}
